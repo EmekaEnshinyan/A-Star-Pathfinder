9424cd37ba52d7e07533e7779480c04e086c43cd
Enshinyan <emeka.enshinyan@gmail.com>
2023-01-05 16:11:14 -0800

    add: grid, start and finish vars

--- A-Star-Pathfinder/Program.cs
+++ A-Star-Pathfinder/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using A_Star_Pathfinder;
+
+List<string> Map = new List<string>
+{
+    "A   ",
+    "-||-",
+    "   B"
+};
+
+var startPosition = new Tile(); 
+//here, Map is a grid made up of a list of strings
+startPosition.Y = Map.FindIndex(x => x.Contains("A"));
+ 
+startPosition.X = Map[startPosition.Y].IndexOf("A");
+
+var endPosition = new Tile();
+endPosition.X = Map.FindIndex((x) => x.Contains("B"));
